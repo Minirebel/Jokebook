036b9f7d4cd3123ab6a7c63bb369000995ff7b92
MiniRebel <Minirebel7@gmail.com>
2024-12-04 08:59:46 +0100

    Add files via upload

---
+++ Jokebook.lua
@@ -0,0 +1,75 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Jokebook: the Balatro community mod.
+--- MOD_ID: The_idea's
+--- MOD_AUTHOR: [Minirebel & the balatro comunity]
+--- MOD_DESCRIPTION: Give me ANY idea and I’ll make a .... out of it
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-0812d]
+--- Prefix: idea
+----------------------------------------------
+------------MOD CODE -------------------------
+
+-----------
+---atlas---
+-----------
+
+SMODS.Atlas {
+    key = "joker",
+    path = "joker_idea.png",
+    px = 71,
+    py = 95
+}
+
+SMODS.Atlas {
+    key = "seal",
+    path = "seal_idea.png",
+    px = 71,
+    py = 95
+}
+
+SMODS.Atlas {
+    key = "Deck",
+    path = "deck_idea.png",
+    px = 71,
+    py = 95
+}
+
+SMODS.Atlas {
+    key = "consumable",
+    path = "consumable_idea.png",
+    px = 71,
+    py = 95
+}
+
+-----------
+---joker---
+-----------
+
+
+
+
+
+
+----------
+---deck---
+----------
+
+
+
+
+
+----------------
+---consumable---
+----------------
+
+
+
+
+
+
+----------
+---seal---
+----------
+
+
+
+
